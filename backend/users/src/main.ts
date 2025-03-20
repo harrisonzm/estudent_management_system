1bfc69fae728f4323fac6a6167930a7a9e43f696
harrisonzm <124377148+harrisonzm@users.noreply.github.com>
2025-03-20 00:32:16 -0500

    init users microservice project with nest
    generation of the microservice with nest and its dependencies(jest, prisma, @nestjs/microservices)

---
+++ backend/users/src/main.ts
@@ -0,0 +1,9 @@
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: { host: '0.0.0.0', port: 4001 },
+  });
+
+  await app.listen();
+}
+bootstrap();
