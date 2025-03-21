f2c7ca996c1106a72837ba60bd33a89388e009a2
harrisonzm <124377148+harrisonzm@users.noreply.github.com>
2025-03-21 04:48:59 -0500

    Delete: unnecesary app files because of modularization

--- backend/client-gateaway/src/app.module.ts
+++ backend/client-gateaway/src/app.module.ts
@@ -1,16 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
-import { AppController } from './app.controller';
-import { UsersService } from './users/users.service';
-import { CoursesService } from './courses/courses.service';
-import { InscriptionsService } from './inscriptions/inscriptions.service';
 import { UsersModule } from './users/users.module';
 import { InscriptionsModule } from './inscriptions/inscriptions.module';
 import { CoursesModule } from './courses/courses.module';
 
 @Module({
   imports: [HttpModule, UsersModule, InscriptionsModule, CoursesModule],
-  controllers: [AppController],
-  providers: [UsersService, CoursesService, InscriptionsService],
 })
 export class AppModule {}
